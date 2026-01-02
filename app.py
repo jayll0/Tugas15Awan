e5d1da44f62d6b8b6bb739161fd383fbe6e0a99d
Alvin Ababil <neoryumasil@gmail.com>
2026-01-02 22:33:16 +0700

    ADD : ROUTING AND PAGES

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from database import Database
+import time
 
 app = Flask(__name__)
+
+database = Database()
 
 @app.route('/')
 def index():
@@ -10,5 +14,21 @@ def index():
 def about():
     return "Ini halaman About (Flask sederhana)"
 
-if __name__ == '__main__':
+# ENDPOINT BARU (bukan /stress)
+@app.route('/cpu-test')
+def cpu_test():
+    start_time = time.time()
+    duration = 300  # 5 menit (300 detik)
+    n = 500         # nilai n untuk loop n^3
+
+    while time.time() - start_time < duration:
+        # Perulangan bersarang n^3 (Cubic Complexity)
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    _ = i * j * k  # Beban CPU
+
+    return render_template('cpu_test.html')
+
+if __name__ == '_main_':
     app.run(debug=True)
